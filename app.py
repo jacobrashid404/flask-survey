48783a0d64efce8f63b253385c7ca5897f0a003a
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-04-25 19:08:19 -0400

    Started route for handling survey answer submissions. WIP.

--- app.py
+++ app.py
@@ -8,32 +8,44 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-responses = [] #store user answers in this list
+responses = []  # store user answers in this list
+
 
 @app.get('/')
 def show_survey_info():
-    #get title and instructions for survey
-    #pass to survey_start.jinja
+    # get title and instructions for survey
+    # pass to survey_start.jinja
     title = survey.title
     instructions = survey.instructions
-    
+
     return render_template(
         "survey_start.jinja",
         title=title,
         instructions=instructions
     )
 
+
 @app.post("/begin")
 def redirect_to_survey():
     return redirect("/questions/0")
 
+
 @app.get("/questions/<int:question_num>")
 def get_question(question_num):
     question = survey.questions[question_num]
-    
+
     return render_template(
         "question.jinja",
         question=question
     )
-    
-    
+
+
+@app.post("/answer")
+def store_and_redirect():
+    """ Store user's response in responses list.
+    Redirect user to next survey question. """  # ask re: docstring indentation
+
+    # append answer to your responses list
+
+    # redirect them to the next question
+    return redirect("/questions/")  # how to increment
