bd77ecb56b0940cdf418fe4f9d0c84eb41314528
Jacob <jacobrashid99@gmail.com>
2024-04-25 12:22:04 -0700

    handle questions, begin and root routes, question WIP

--- app.py
+++ app.py
@@ -7,3 +7,33 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+responses = [] #store user answers in this list
+
+@app.get('/')
+def show_survey_info():
+    #get title and instructions for survey
+    #pass to survey_start.jinja
+    title = survey.title
+    instructions = survey.instructions
+    
+    return render_template(
+        "survey_start.jinja",
+        title=title,
+        instructions=instructions
+    )
+
+@app.post("/begin")
+def redirect_to_survey():
+    return redirect("/questions/0")
+
+@app.get("/questions/<int:question_num>")
+def get_question(question_num):
+    question = survey.questions[question_num]
+    
+    return render_template(
+        "question.jinja",
+        question=question
+    )
+    
+    
